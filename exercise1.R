248d0cf82f030657728ce3ca2ab7f6ef4943e170
kelechichima13 <kelechi.chima@ndm.ox.ac.uk>
2024-01-29 12:42:41 +0000

    excercises

--- exercise1.R
+++ exercise1.R
@@ -1,2 +1,12 @@
 #first excercise code to read the .dat file
 ba <- read.table(file = "https://raw.githubusercontent.com/OxfordIHTM/teaching_datasets/main/ba.dat", header = TRUE, sep = ' ')
+calculate_mean <- function(x,y){
+  avrg = (x+y)/2
+  avrg
+}
+ba$mean <- calculate_mean(ba$Wright,ba$Mini)
+calculate_diff <- function(x,y){
+  diff = x-y
+  diff
+}
+ba$diff <- calculate_diff(ba$Wright,ba$Mini)
