0e19cabfcda3aa2c7e59e4152c96c45a79d200dd
Mariano Ratto <mratto.ib@gmail.com>
2024-01-29 12:49:23 +0000

    Commit again

--- exercise1.R
+++ exercise1.R
@@ -1,7 +1,11 @@
 ba = read.table("https://raw.githubusercontent.com/OxfordIHTM/teaching_datasets/main/ba.dat",header=TRUE)
 
+BandA =function(W,M){
+ 
+  meanW= mean(W)
+  meanM= mean(M)
+  dif=W-M
+  meanD=mean(dif)
+   
+}
 
-meanW= mean(ba$Wright)
-meanM= mean(ba$Mini)
-dif=ba$Wright-ba$Mini
-meanD=mean(dif)
