6e951f001f3ce9de83a25f835a0a92609532b9e9
kelechichima13 <kelechi.chima@ndm.ox.ac.uk>
2024-01-29 13:17:56 +0000

    task 2

--- exercise1.R
+++ exercise1.R
@@ -13,3 +13,17 @@ ba$diff <- calculate_diff(ba$Wright,ba$Mini)
 ba$diffmean <- mean(ba$diff)
 ?mean
 
+#creating functions for the upper and lower intervals
+conf_intupp <- function(x,y){
+  upper = x+1.96*sd(y)
+  upper
+}
+
+conf_intlow <- function(x,y){
+  lower = x-1.96*sd(y)
+  lower
+}
+
+#creating corresponding columns for the intervals
+ba$upper <- conf_intupp(ba$diffmean,ba$diff)
+ba$lower <- conf_intlow(ba$diffmean,ba$diff)
