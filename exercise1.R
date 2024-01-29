f28f551b4be8ee6d7369600a01f31544f933523f
Jillian Francise Lee <jillianfrancise@gmail.com>
2024-01-29 12:40:24 +0000

    with task 2

--- exercise1.R
+++ exercise1.R
@@ -1,3 +1,5 @@
 ?read.table
 ba <- read.table(file = url("https://raw.githubusercontent.com/OxfordIHTM/teaching_datasets/main/ba.dat"), header = TRUE, sep = " ")
 
+meanWright <- mean(ba$Wright)
+meanMini <- mean(ba$Mini)
