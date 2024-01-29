1731f3d45aca9d05908ab6d82bf47858a94e7d71
Jillian Francise Lee <jillianfrancise@gmail.com>
2024-01-29 13:11:34 +0000

    task 2 final-ish

--- exercise1.R
+++ exercise1.R
@@ -8,4 +8,5 @@ ba[,3] = ba$Wright-ba$Mini
 colnames(ba)[colnames(ba) == "V3"] <- "Difference"
 
 meanDifference <- mean(ba$Difference)
-
+upperCI <- meanDifference + 1.96 * sd(ba$Difference)
+lowerCI <- meanDifference - 1.96 * sd(ba$Difference)
