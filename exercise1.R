98e1093cca2f600ccfcda5d08668c3f8ab598689
Mariano Ratto <mratto.ib@gmail.com>
2024-01-29 13:09:03 +0000

    Task 2 done

--- exercise1.R
+++ exercise1.R
@@ -1,11 +1,14 @@
 ba = read.table("https://raw.githubusercontent.com/OxfordIHTM/teaching_datasets/main/ba.dat",header=TRUE)
 
 BandA =function(W,M){
- 
-  meanW= mean(W)
-  meanM= mean(M)
-  dif=W-M
-  meanD=mean(dif)
+  meanD=mean(abs(W-M))
+  SDD=sd(abs(W-M))
+  L=meanD-1.96*SDD
+  U=meanD+1.96*SDD
+  
+  return (list(meanD,L,U))
    
 }
+
+H = BandA(ba$Wright,ba$Mini)
 
