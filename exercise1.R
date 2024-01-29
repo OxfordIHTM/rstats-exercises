9d089b6f264e2880456e98c35506240a08c2408f
Jillian Francise Lee <jillianfrancise@gmail.com>
2024-01-29 13:03:35 +0000

    with task 2 i-iii

--- exercise1.R
+++ exercise1.R
@@ -3,3 +3,9 @@ ba <- read.table(file = url("https://raw.githubusercontent.com/OxfordIHTM/teachi
 
 meanWright <- mean(ba$Wright)
 meanMini <- mean(ba$Mini)
+
+ba[,3] = ba$Wright-ba$Mini
+colnames(ba)[colnames(ba) == "V3"] <- "Difference"
+
+meanDifference <- mean(ba$Difference)
+
