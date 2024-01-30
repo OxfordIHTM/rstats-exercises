7804d51934bb5bdb9b12b27b3c29f2be5a484615
Clifford Cofie <cofieacademic@gmail.com>
2024-01-30 03:59:10 +0000

    another solution as requested from team naruto

--- exercise1.R
+++ exercise1.R
@@ -1,2 +1,29 @@
 ba = read.table("https://raw.githubusercontent.com/OxfordIHTM/teaching_datasets/main/ba.dat",header=TRUE)
-meanW= mean(ba$Wright)
+
+##mean difference 
+differences <- ba$Wright - ba$Mini
+mean_difference <- mean(differences)
+
+calculate_limits_of_agreement <- function(wright, mini){
+  
+##difference of per subject measurements
+differences <- wright - mini
+
+##calculate mean difference
+mean_difference <- mean(differences)
+
+##calculate sd of difference
+sd_difference <- sd(differences)
+
+limit_multiplier <- 1.96
+
+
+lower_limit = mean_difference - (limit_multiplier * sd_difference)
+upper_limit = mean_difference + (limit_multiplier * sd_difference)
+
+return(c(lower_limit,upper_limit))
+}
+
+conf_interval <- calculate_limits_of_agreement(ba$Wright, ba$Mini)
+
+
