a9784b142813983edff24567c9791762c01c91e0
Rasika Bombatkar <rasika.bombatkar@gtc.ox.ac.uk>
2024-02-04 18:11:04 +0000

    Exercise 2 Task 1 and 2

--- Exercise2-Group Hinata.R
+++ Exercise2-Group Hinata.R
@@ -1,17 +1,21 @@
 # Install and load necessary packages
 install.packages("foreign")
 library(foreign)
+install.packages("gtsummary")
+library(gtsummary)
+library(ggplot2)
 
 # Read the fem.dat dataset
-url <- "https://raw.githubusercontent.com/OxfordIHTM/teaching_datasets/main/fem.dat"
-fem_data <- read.table(url, header = TRUE)
-
-# Convert relevant columns to numeric
-numeric_columns <- c("AGE", "IQ", "WT")
-fem_data[, numeric_columns] <- lapply(fem_data[, numeric_columns], as.numeric)
+fem_data <- read.table("https://raw.githubusercontent.com/OxfordIHTM/teaching_datasets/main/fem.dat", header = TRUE)
 
 # Summary of variables for AGE, IQ, and WT
-summary(fem_data[, numeric_columns])
+summary(fem_data$AGE)
+summary(fem_data$IQ)
+summary(fem_data$WT)
+
+# Summary in tabular form using gtsummary
+summary_table <- tbl_summary(fem_data)
+summary_table
 
 # Create boxplots for AGE, IQ, and WT
 par(mfrow=c(1,3))
