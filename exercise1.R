88bc416257458384aecf62ec8a36e3ab0b82ccfe
Claudia Lucia Vidal Cuellar <claudia.vidal.c@upch.pe>
2024-02-06 10:59:44 +0000

    Bland Altman Plot and initial report

--- exercise1.R
+++ exercise1.R
@@ -1,2 +1,24 @@
+library(ggplot2)
+
 ba = read.table("https://raw.githubusercontent.com/OxfordIHTM/teaching_datasets/main/ba.dat",header=TRUE)
-meanW= mean(ba$Wright)
+
+average = rowMeans(ba)
+ba$average = average
+#Mean differences 
+difference <- ba$Wright - ba$Mini
+ba$difference = difference
+mean_difference = mean(difference)
+
+#Confidence interval
+lower <- mean_difference - 1.96*sd(ba$difference)
+upper <- mean_difference + 1.96*sd(ba$difference)
+
+#Create Bland Altman Plot
+ggplot(ba, aes(x = average, y = difference)) +
+  geom_point() +
+  geom_hline(yintercept = mean_difference) +
+  geom_hline(yintercept = lower, color = "red", linetype="dashed") +
+  geom_hline(yintercept = upper, color = "red", linetype="dashed") +
+  ylab("PEFR (l/min) Mini Wright meter - Wright meter ") +
+  xlab("PEFR (l/min) Mini Wright meter + Wright meter / 2")
+
